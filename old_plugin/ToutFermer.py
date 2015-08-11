f4cecb5ee535ba45ac6b7eee3cd9217012a7fee5
Ludovic Delauné <ludovic.delaune@oslandia.com>
2015-08-11 16:29:11 +0200

    delete old plugin code

--- old_plugin/ToutFermer.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from qgis.core import QgsMapLayerRegistry
-
-
-class action():
-    def __init__(self, iface):
-        # Fermer toutes les couches d'un coup :
-        for L in iface.legendInterface().layers():
-            QgsMapLayerRegistry.instance().removeMapLayer(L.id())
