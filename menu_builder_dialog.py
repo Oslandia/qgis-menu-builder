d0f4038858669c1137cd034e5612d3ffb052ca44
Ludovic Delauné <ludovic.delaune@oslandia.com>
2015-08-13 23:32:00 +0200

    clear combo schema

--- menu_builder_dialog.py
+++ menu_builder_dialog.py
@@ -196,6 +196,7 @@ class MenuBuilderDialog(QDialog, FORM_CLASS):
         settings.beginGroup("/PostgreSQL/connections")
         keys = settings.childGroups()
         self.combo_database.clear()
+        self.combo_schema.clear()
         self.combo_database.addItems(keys)
         self.combo_database.setCurrentIndex(-1)
         settings.endGroup()
